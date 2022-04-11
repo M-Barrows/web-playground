7039015d6dbbcee3ee6c8569ff6f64e6dd839190
Michael Barrows <m.barrows@live.com>
2022-04-10 22:31:31 -0400

    feat: Leaderboard and Jinja templates
    Added a scafolded leaderboard page
    
    created header and footer templates for modularization

--- src/app.py
+++ src/app.py
@@ -10,5 +10,38 @@ def hello():
 def home():
     return render_template('index.html')
 
+@app.route('/leaderboard')
+def leaderboard():
+    visits = [
+        {
+            'name':'Michael',
+            'visits':3
+        },
+        {
+            'name':'Joe',
+            'visits':2
+        },
+        {
+            'name':'Billy',
+            'visits':1
+        }
+    ]
+
+    streak = [
+        {
+            'name':'Michael',
+            'streak':2
+        },
+        {
+            'name':'Joe',
+            'streak':1
+        },
+        {
+            'name':'Billy',
+            'streak':3
+        }
+    ]
+
+    return render_template('leaderboard.html',visits = visits, streak = streak)
 if __name__ == '__main__': 
     app.run(debug=True)
