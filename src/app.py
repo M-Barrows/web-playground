e0b9c73a082a21d42061a8aca09155485870d6b6
Michael Barrows <m.barrows@live.com>
2022-04-17 22:21:06 -0400

    adding cicd endpoint

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import git
 
 app = Flask(__name__)
 
@@ -9,6 +10,16 @@ def hello():
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/push-update', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/codeandcoffee/home')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 @app.route('/leaderboard')
 def leaderboard():
